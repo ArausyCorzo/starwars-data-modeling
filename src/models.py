1a25beb59d939cfe3f9d95fea40d12450bfe1f21
ArausyCorzo <arausyncc@gmail.com>
2021-12-11 22:25:35 +0000

    Creo que ya termine pero no se, ayuda:D

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -10,13 +10,47 @@ Base = declarative_base()
 
 class Users(Base):
     __tablename__ = 'users'
-    user_id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
     user_name= Column(String(8), nullable=False)
     first_name= Column(String(40), nullable=False)
     last_name= Column (String(40), nullable=False)
     email= Column(String(12), unique=True)
-    
+    favorites= relationship("Favorites", back_populates="parent")
 
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id= Column(Integer, primary_key=True)
+    element_name= Column(String(20), nullable=False)
+    amount= Column(Integer, default=0)
+    users_id= Column(Integer, ForeignKey("users.id"))
+    users= relationship("Users", back_populates="children")
+    # planets= relationship("Planets")
+    # characters= relationship("Characters")
+
+class Planets(Base):
+    __tablename__= 'planets'
+    id= Column(Integer, primary_key=True)
+    name= Column(String(20), nullable=False)
+    population= Column(Float)
+    terrain= Column(String, nullable=False)
+    favorites_id= Column(Integer, ForeignKey("favorites.id"))
+
+class Characters(Base):
+    __tablename__= 'characters'
+    id= Column(Integer, primary_key=True)
+    name= Column(String(10), nullable=False)
+    hair_color= Column(String(8), nullable=False)
+    eyes_color= Column(String(8), nullable=False)
+    description= Column(String(200), nullable=False)
+    favorites_id= Column(Integer, ForeignKey("favorites.id"))
+
+class Vehicles(Base):
+    __tablename__= 'Vehicles'
+    id= Column(Integer, primary_key=True)
+    name= Column(String(10), nullable=False)
+    name_pilot= Column(String(40), nullable=False)
+    passenger= Column(Integer)
+    favorites_id= Column(Integer, ForeignKey("favorites.id"))
 
 
 
