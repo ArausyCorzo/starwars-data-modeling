0fac049e0f70e0df583a33ad82a8367ba599cc5e
ArausyCorzo <arausyncc@gmail.com>
2021-12-11 20:41:57 +0000

    actual

--- src/models.py
+++ src/models.py
@@ -10,26 +10,15 @@ Base = declarative_base()
 
 class Users(Base):
     __tablename__ = 'users'
+    user_id = Column(Integer, primary_key=True)
     user_name= Column(String(8), nullable=False)
-    id = Column(Integer, primary_key=True)
+    first_name= Column(String(40), nullable=False)
+    last_name= Column (String(40), nullable=False)
+    email= Column(String(12), unique=True)
+    
 
-# class Person(Base):
-#     __tablename__ = 'person'
-#     # Here we define columns for the table person
-#     # Notice that each column is also a normal Python instance attribute.
-#     id = Column(Integer, primary_key=True)
-#     name = Column(String(250), nullable=False)
 
-# class Address(Base):
-#     __tablename__ = 'address'
-#     # Here we define columns for the table address.
-#     # Notice that each column is also a normal Python instance attribute.
-#     id = Column(Integer, primary_key=True)
-#     street_name = Column(String(250))
-#     street_number = Column(String(250))
-#     post_code = Column(String(250), nullable=False)
-#     person_id = Column(Integer, ForeignKey('person.id'))
-#     person = relationship(Person)
+
 
     def to_dict(self):
         return {}
